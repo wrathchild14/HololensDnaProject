d38550de61ea117eb09a8cfec99c7d9288c7d0db
wrathchild14 <jovanprodanov123@gmail.com>
2022-12-10 23:49:12 +0100

    add sliders for changing scale and position

--- Assets/Scripts/Utility/ScalePositionController.cs
+++ Assets/Scripts/Utility/ScalePositionController.cs
@@ -1,14 +1,24 @@
 using Microsoft.MixedReality.Toolkit.UI;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Utility
 {
     public class ScalePositionController : MonoBehaviour
     {
-        public void OnSliderUpdated(SliderEventData eventData)
+        [FormerlySerializedAs("multiplier")] [SerializeField] private float posMultiplier = 10;
+
+        public void OnSliderScaleUpdated(SliderEventData eventData)
         {
-            var sliderSliderValue = eventData.Slider.SliderValue;
+            var sliderSliderValue = eventData.Slider.SliderValue + 0.5f;
             transform.localScale = new Vector3(sliderSliderValue, sliderSliderValue, sliderSliderValue);
+        }
+
+        public void OnSliderPositionUpdated(SliderEventData eventData)
+        {
+            var sliderSliderValue = eventData.Slider.SliderValue + 0.5f;
+            transform.position =
+                new Vector3(transform.position.x, transform.position.y, sliderSliderValue * posMultiplier - 100f);
         }
     }
 }
