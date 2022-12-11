8e47c0a07b29bddbeef575421dc71d24c3b8f00d
wrathchild14 <jovanprodanov123@gmail.com>
2022-12-11 18:43:15 +0100

    add new simplified prefab, fix slider script

--- Assets/Scripts/Utility/ScalePositionController.cs
+++ Assets/Scripts/Utility/ScalePositionController.cs
@@ -7,11 +7,13 @@ namespace Utility
     public class ScalePositionController : MonoBehaviour
     {
         [FormerlySerializedAs("multiplier")] [SerializeField] private float posMultiplier = 10;
+        [SerializeField] private float scaleMultiplier = 3f;
 
         public void OnSliderScaleUpdated(SliderEventData eventData)
         {
-            var sliderSliderValue = eventData.Slider.SliderValue + 0.5f;
-            transform.localScale = new Vector3(sliderSliderValue, sliderSliderValue, sliderSliderValue);
+            var sliderSliderValue = eventData.Slider.SliderValue;
+            transform.localScale = new Vector3(sliderSliderValue * scaleMultiplier, sliderSliderValue * scaleMultiplier,
+                sliderSliderValue * scaleMultiplier);
         }
 
         public void OnSliderPositionUpdated(SliderEventData eventData)
