a3de3440d85c508748aa653098a9d53a9b2cde22
wrathchild14 <jovanprodanov123@gmail.com>
2023-01-13 01:01:47 +0100

    add base for interaction

--- Assets/Scripts/MeshOutline.cs
+++ Assets/Scripts/MeshOutline.cs
@@ -2,146 +2,144 @@
 // Licensed under the MIT License.
 
 using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.Utilities;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-namespace Microsoft.MixedReality.Toolkit.Utilities
+/// <summary>
+///     Component which can be used to render an outline around a mesh renderer. Enabling this component introduces an
+///     additional render pass
+///     of the object being outlined, but is designed to run performantly on mobile Mixed Reality devices and does not
+///     utilize any post processes.
+///     This behavior is designed to be used in conjunction with the MRTK/Standard shader. Limitations of this effect
+///     include it not working well
+///     on objects which are not watertight (or required to be two sided) and depth sorting issues can occur on overlapping
+///     objects.
+/// </summary>
+[RequireComponent(typeof(MeshRenderer))]
+[AddComponentMenu("Scripts/MRTK/Core/MeshOutline")]
+public class MeshOutline : BaseMeshOutline
 {
+    private const string vertexExtrusionKeyword = "_VERTEX_EXTRUSION";
+    private const string vertexExtrusionSmoothNormalsKeyword = "_VERTEX_EXTRUSION_SMOOTH_NORMALS";
+    private const string vertexExtrusionValueName = "_VertexExtrusionValue";
+    private MeshSmoother createdMeshSmoother;
+    private Material[] defaultMaterials;
+
+    private MeshRenderer meshRenderer;
+    private MaterialPropertyBlock propertyBlock;
+    private int vertexExtrusionValueID;
+
     /// <summary>
-    /// Component which can be used to render an outline around a mesh renderer. Enabling this component introduces an additional render pass 
-    /// of the object being outlined, but is designed to run performantly on mobile Mixed Reality devices and does not utilize any post processes.
-    /// This behavior is designed to be used in conjunction with the MRTK/Standard shader. Limitations of this effect include it not working well 
-    /// on objects which are not watertight (or required to be two sided) and depth sorting issues can occur on overlapping objects.
+    ///     Searches for the minimum render queue value in a list of materials.
     /// </summary>
-    [RequireComponent(typeof(MeshRenderer))]
-    [AddComponentMenu("Scripts/MRTK/Core/MeshOutline")]
-    public class MeshOutline : BaseMeshOutline
+    /// <param name="materials">The list of materials to search.</param>
+    /// <returns>The minimum render queue value.</returns>
+    private static int GetMinRenderQueue(Material[] materials)
     {
-        private const string vertexExtrusionKeyword = "_VERTEX_EXTRUSION";
-        private const string vertexExtrusionSmoothNormalsKeyword = "_VERTEX_EXTRUSION_SMOOTH_NORMALS";
-        private const string vertexExtrusionValueName = "_VertexExtrusionValue";
+        var min = int.MaxValue;
 
-        private MeshRenderer meshRenderer = null;
-        private MaterialPropertyBlock propertyBlock = null;
-        private int vertexExtrusionValueID = 0;
-        private Material[] defaultMaterials = null;
-        private MeshSmoother createdMeshSmoother = null;
+        foreach (var material in materials)
+            if (material != null)
+                min = Mathf.Min(min, material.renderQueue);
 
-        #region MonoBehaviour Implementation
+        if (min == int.MaxValue) min = (int)RenderQueue.Background;
 
-        /// <summary>
-        /// Gathers initial render state.
-        /// </summary>
-        private void Awake()
+        return min;
+    }
+
+    #region MonoBehaviour Implementation
+
+    /// <summary>
+    ///     Gathers initial render state.
+    /// </summary>
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        propertyBlock = new MaterialPropertyBlock();
+        vertexExtrusionValueID = Shader.PropertyToID(vertexExtrusionValueName);
+        defaultMaterials = meshRenderer.sharedMaterials;
+    }
+
+    /// <summary>
+    ///     Enables the outline.
+    /// </summary>
+    private void OnEnable()
+    {
+        ApplyOutlineMaterial();
+    }
+
+    /// <summary>
+    ///     Resets the renderer materials to the default settings.
+    /// </summary>
+    private void OnDisable()
+    {
+        meshRenderer.materials = defaultMaterials;
+    }
+
+    /// <summary>
+    ///     Removes any components this component has created.
+    /// </summary>
+    private void OnDestroy()
+    {
+        Destroy(createdMeshSmoother);
+    }
+
+    #endregion MonoBehaviour Implementation
+
+    #region BaseMeshOutline Implementation
+
+    /// <summary>
+    ///     Prepares and applies the current outline material to the renderer.
+    /// </summary>
+    protected override void ApplyOutlineMaterial()
+    {
+        if (outlineMaterial != null && meshRenderer != null)
         {
-            meshRenderer = GetComponent<MeshRenderer>();
-            propertyBlock = new MaterialPropertyBlock();
-            vertexExtrusionValueID = Shader.PropertyToID(vertexExtrusionValueName);
-            defaultMaterials = meshRenderer.sharedMaterials;
-        }
+            Debug.AssertFormat(outlineMaterial.IsKeywordEnabled(vertexExtrusionKeyword),
+                "The material \"{0}\" does not have vertex extrusion enabled, no outline will be rendered.",
+                outlineMaterial.name);
 
-        /// <summary>
-        /// Enables the outline.
-        /// </summary>
-        private void OnEnable()
-        {
-            ApplyOutlineMaterial();
-        }
+            // Ensure that the outline material always renders before the default materials.
+            outlineMaterial.renderQueue = GetMinRenderQueue(defaultMaterials) - 1;
 
-        /// <summary>
-        /// Resets the renderer materials to the default settings.
-        /// </summary>
-        private void OnDisable()
-        {
-            meshRenderer.materials = defaultMaterials;
-        }
-
-        /// <summary>
-        /// Removes any components this component has created.
-        /// </summary>
-        private void OnDestroy()
-        {
-            Destroy(createdMeshSmoother);
-        }
-
-        #endregion MonoBehaviour Implementation
-
-        #region BaseMeshOutline Implementation
-
-        /// <summary>
-        /// Prepares and applies the current outline material to the renderer.
-        /// </summary>
-        protected override void ApplyOutlineMaterial()
-        {
-            if (outlineMaterial != null && meshRenderer != null)
+            // If smooth normals are requested, make sure the mesh has smooth normals.
+            if (outlineMaterial.IsKeywordEnabled(vertexExtrusionSmoothNormalsKeyword))
             {
-                Debug.AssertFormat(outlineMaterial.IsKeywordEnabled(vertexExtrusionKeyword),
-                                   "The material \"{0}\" does not have vertex extrusion enabled, no outline will be rendered.", outlineMaterial.name);
+                var meshSmoother = createdMeshSmoother == null
+                    ? gameObject.GetComponent<MeshSmoother>()
+                    : createdMeshSmoother;
 
-                // Ensure that the outline material always renders before the default materials.
-                outlineMaterial.renderQueue = GetMinRenderQueue(defaultMaterials) - 1;
-
-                // If smooth normals are requested, make sure the mesh has smooth normals.
-                if (outlineMaterial.IsKeywordEnabled(vertexExtrusionSmoothNormalsKeyword))
+                if (meshSmoother == null)
                 {
-                    var meshSmoother = (createdMeshSmoother == null) ? gameObject.GetComponent<MeshSmoother>() : createdMeshSmoother;
-
-                    if (meshSmoother == null)
-                    {
-                        createdMeshSmoother = gameObject.AddComponent<MeshSmoother>();
-                        meshSmoother = createdMeshSmoother;
-                    }
-
-                    meshSmoother.SmoothNormals();
+                    createdMeshSmoother = gameObject.AddComponent<MeshSmoother>();
+                    meshSmoother = createdMeshSmoother;
                 }
 
-                ApplyOutlineWidth();
-
-                // Add the outline material as another material pass.
-                var materials = new List<Material>(defaultMaterials);
-                materials.Add(outlineMaterial);
-                meshRenderer.materials = materials.ToArray();
-            }
-        }
-
-        /// <summary>
-        /// Updates the current vertex extrusion value used by the shader. 
-        /// </summary>
-        protected override void ApplyOutlineWidth()
-        {
-            if (meshRenderer != null && propertyBlock != null)
-            {
-                meshRenderer.GetPropertyBlock(propertyBlock);
-                propertyBlock.SetFloat(vertexExtrusionValueName, outlineWidth);
-                meshRenderer.SetPropertyBlock(propertyBlock);
-            }
-        }
-
-        #endregion BaseMeshOutline Implementation
-
-        /// <summary>
-        /// Searches for the minimum render queue value in a list of materials.
-        /// </summary>
-        /// <param name="materials">The list of materials to search.</param>
-        /// <returns>The minimum render queue value.</returns>
-        private static int GetMinRenderQueue(Material[] materials)
-        {
-            var min = int.MaxValue;
-
-            foreach (var material in materials)
-            {
-                if (material != null)
-                {
-                    min = Mathf.Min(min, material.renderQueue);
-                }
+                meshSmoother.SmoothNormals();
             }
 
-            if (min == int.MaxValue)
-            {
-                min = (int)RenderQueue.Background;
-            }
+            ApplyOutlineWidth();
 
-            return min;
+            // Add the outline material as another material pass.
+            var materials = new List<Material>(defaultMaterials);
+            materials.Add(outlineMaterial);
+            meshRenderer.materials = materials.ToArray();
         }
     }
+
+    /// <summary>
+    ///     Updates the current vertex extrusion value used by the shader.
+    /// </summary>
+    protected override void ApplyOutlineWidth()
+    {
+        if (meshRenderer != null && propertyBlock != null)
+        {
+            meshRenderer.GetPropertyBlock(propertyBlock);
+            propertyBlock.SetFloat(vertexExtrusionValueName, outlineWidth);
+            meshRenderer.SetPropertyBlock(propertyBlock);
+        }
+    }
+
+    #endregion BaseMeshOutline Implementation
 }
--- Assets/Scripts/NucleotideController.cs
+++ Assets/Scripts/NucleotideController.cs
@@ -1,29 +1,60 @@
+using System;
+using System.IO;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.UI;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class NucleotideController : MonoBehaviour
+public class NucleotideController : MonoBehaviour, IMixedRealityPointerHandler
 {
+    private MeshOutline _outline;
+    public string filename = "test.txt";
+    private string _path;
+
     private void Start()
     {
-        var path = Application.persistentDataPath;
+        _path = Path.Combine(Application.persistentDataPath, filename);
         // read information from some file in this path
+        _outline = GetComponent<MeshOutline>();
+    }
+
+    public void OnInteractionStarted()
+    {
+        Debug.Log("interaction started");
+
+        var data = "I interacted with " + gameObject.name;
+        File.AppendAllText(_path, data);
+        _outline.enabled = true;
+    }
+
+    public void OnInteractionEnded()
+    {
+        // _outline.enabled = false;
+        Debug.Log("interaction ended");
     }
 
     private void Update()
     {
         // when gazed upon display information
-        DisplayInformation();
-        
         // when pinched save information to path
-        SaveInformation();
     }
 
-    private void SaveInformation()
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        Debug.Log("pointer dow");
+        OnInteractionStarted();
     }
 
-    private void DisplayInformation()
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+        OnInteractionEnded();
+    }
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
     }
 }
