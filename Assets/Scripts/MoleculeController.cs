40be7f0a1828766c0f29e14a7dbab4dc7a8c49ae
wrathchild14 <jovanprodanov123@gmail.com>
2023-01-13 14:49:48 +0100

    rename and move controller script

---
+++ Assets/Scripts/MoleculeController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.IO;
+using Microsoft.MixedReality.Toolkit.Input;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MoleculeController : MonoBehaviour, IMixedRealityPointerHandler
+    {
+        public TextMeshPro readDataText;
+        public string fileName = "names.txt";
+        private bool _saved = false;
+        private string _filePath;
+
+        private MeshOutline _outline;
+        private DateTime _timePassed;
+
+        private void Start()
+        {
+            _filePath = Path.Combine(Application.persistentDataPath, fileName);
+            // read information from some file in this path
+            _outline = GetComponent<MeshOutline>();
+        }
+
+        public void OnPointerDown(MixedRealityPointerEventData eventData)
+        {
+            if (!_saved)
+            {
+                _outline.enabled = true;
+                //Debug.Log("interaction started");
+
+                _timePassed = DateTime.Now;
+            }
+        }
+
+        public void OnPointerDragged(MixedRealityPointerEventData eventData)
+        {
+        }
+
+        public void OnPointerUp(MixedRealityPointerEventData eventData)
+        {
+            if (!_saved)
+            {
+                var duration = DateTime.Now.Subtract(_timePassed);
+                if (duration.Seconds >= 2)
+                {
+                    var data = gameObject.name + ", ";
+                    File.AppendAllText(_filePath, data);
+                    _saved = true;
+                }
+                else
+                {
+                    _outline.enabled = false;
+                }
+            }
+        }
+
+        public void OnPointerClicked(MixedRealityPointerEventData eventData)
+        {
+            if (_outline.enabled && _saved) readDataText.text = File.ReadAllText(_filePath);
+        }
+    }
+}
